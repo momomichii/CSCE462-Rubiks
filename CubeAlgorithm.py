e1524b23a39fba231a4893c98f550f2b25d0e48e
wilsonjoshua1 <jwil1010@tamu.edu>
2024-11-21 13:45:56 -0600

    Colors GUI

--- CubeAlgorithm.py
+++ CubeAlgorithm.py
@@ -5,10 +5,17 @@ def scan_cube():
     # Replace this with your camera scanning logic using OpenCV
     # For example: return a string like 'UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB'
   
-    cube_colors = "URDBUUUDLFRLBRRRFFFLDUFDUDDRRBDDURLDLULLLLBFFBFBBBBRFU" #example of it solved
+    cube_colors = "UUUUUUBFLDLRRRFURBDLBDFFFFLDUFDDBDDLRRRLLRRLLBBFDBBUBF" #example of it solved
     # the colors go from left to right and top and down, starts with White(U), red(R), Green(F), Yellow(D), orange (L), blue(B)
     #U1U2U3U4U5U6U7U8U9 R1R2R3R4R5R6R7R8R9 F1F2F3F4F5F6F7F8F9 D1D2D3D4D5D6D7D8D9 L1L2L3L4L5L6L7L8L9 B1B2B3B4B5B6B7B8B9
     # the input will use U, R, F, D, L, B but in the order above to represent the colors of the cube
+    # white= "UUUUUUBFL"
+    # red= "DLRRRFURB"
+    # green= "DLBDFFFFL"
+    # yellow= "DUFDDBDDL"
+    # orange= "RRRLLRRLL"
+    # blue= "BBFDBBUBF"
+    
     return cube_colors
 
 def solve_rubik_cube():
